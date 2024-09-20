22052748ef32b11ec79a25dd5b601cf0c973043c
LuisOrlandoSifuentes <a.22308051281313@cbtis128.edu.mx>
2024-09-20 10:03:27 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,57 @@
+print("--Act 9 clases v3--")
+print("--Luis Sifuentes-Mat: 22308051281313--")
+
+# zona de class
+class Operadores1313: 
+# zona de funciones
+    # SUMA
+    def suma1313(self,L,S):
+            s1313=L+S
+            print(f"La suma de {L} + {S} = {s1313}")
+    # RESTA
+    def resta1313(self,L,S):
+            r1313=L-S
+            print(f"La resta de {L} - {S} = {r1313}")
+    # MULTIPLICACION
+    def multi1313(self,L,S):
+            m1313=L*S
+            print(f"La multiplicacion de {L} * {S} = {m1313}")
+    # DIVISION
+    def div1313(self,L,S):
+            d1313=L/S
+            print(f"La division de {L} / {S} = {d1313}")
+    # MODULO
+    def mod1313(self,L,S):
+            M1313=L%S
+            print(f"El modulo de {L} % {S} = {M1313}")
+    # EXPONENTE
+    def expo1313(self,L,S):
+            e1313=L**S
+            print(f"El exponente de {L} ** {S} = {e1313}")
+    # COCIENTE
+    def cos1313(self,L,S):
+            c1313=L//S
+            print(f"El cociente de {L} // {S} = {c1313}")
+    #zona de creacion del objeto
+operaSIF=Operadores1313()
+    #zona del uso de objetos
+print("--Funcion suma--")
+operaSIF.suma1313(5,7)
+
+print("--Funcion resta--")
+operaSIF.resta1313(6,8)
+
+print("--Funcion multiplicacion--")
+operaSIF.multi1313(5,8)
+
+print("--Funcion division--")
+operaSIF.div1313(9,4)
+
+print("--Funcion modulo--")
+operaSIF.mod1313(2,7)
+
+print("--Funcion exponente--")
+operaSIF.expo1313(9,2)
+
+print("--Funcion cociente--")
+operaSIF.cos1313(5,4)
